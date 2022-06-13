f2becbe92d846f5b0bdf286d3000fa882b9ef48a
user00777777 <misterborman@gmail.com>
2022-06-13 20:43:05 +0300

    b

--- src/redux/posts-reduser.js
+++ src/redux/posts-reduser.js
@@ -1,22 +1,21 @@
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const ADD_POST = "ADD-POST";
-// let initialState = {
-//   post: [
-//     { id: 1, message: "привет", likeCount: 1 },
-//     { id: 2, message: "hi", likeCount: 12 },
-//     { id: 3, message: "Hello", likeCount: 13 },
-//     { id: 4, message: "Goodmorning", likeCount: 14 },
-//   ],
-//   newPostText: " ",
-// };
+let initialState = {
+  post: [
+    { id: 1, message: "привет", likeCount: 1 },
+    { id: 2, message: "hi", likeCount: 12 },
+    { id: 3, message: "Hello", likeCount: 13 },
+    { id: 4, message: "Goodmorning", likeCount: 14 },
+  ],
+  newPostText: "",
+};
 
-const postsReduser = (state, action) => {
+const postsReduser = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let tex = state.newPostText;
       state.newPostText = "";
       state.post.push({ message: tex });
-
       return state;
     case UPDATE_NEW_POST_TEXT:
       state.newPostText = action.text;
